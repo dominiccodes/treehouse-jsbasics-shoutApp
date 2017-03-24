c8dbeadcc5c2cf0f657003c137f28ddaed5e0c6d
domcodes <email@domcodes.com>
2017-03-24 15:17:32 +0000

    Setup files and instructions

---
+++ js/script.js
@@ -0,0 +1,44 @@
+//When page loads a prompt appears, 
+//User types something to shout
+
+
+
+//convert response to ALL CAPS + !
+
+
+
+// Send message back to them
+
+
+
+
+
+
+
+
+
+
+//  LESSON NOTES.  String Properties and Methods
+
+
+
+
+
+//.length returns the number of characters inside a string
+//
+//var str = '9 letters';
+//alert( str.length ); 
+// opens an alert dialog with the number 9
+
+//.toUpperCase() returns a copy of a string with all uppercase letters
+//
+//var str = 'upper';
+//alert( str.toUpperCase() ); 
+// opens an alert dialog with the string 'UPPER'
+
+//.toLowerCase() returns a copy of a string with all lowercase letters
+//
+//var str = 'LOWER';
+//alert( str.toLowerCase() ); 
+// opens an alert dialog with the string 'lower'
+
